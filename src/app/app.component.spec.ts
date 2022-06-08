5fa6676342b108a4ae18aaf013dfe4758db8dca8
Dima Komoff <dkomo@softserveinc.com>
2022-06-08 12:52:31 +0300

    renamed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,9 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have as title "Angular deployment"', () => {
+  it('should have as title "Tic Tac Toe"', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular deployment');
+    expect(app.title).toEqual('Tic Tac Toe');
   });
 });
